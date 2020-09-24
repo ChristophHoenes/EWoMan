47c1fe0d564f319e627ebf35a5cb3a30263e3d40
Tamara Czinczoll <lgpu0213@test3.irc.sara.nl.irc.sara.nl>
2020-09-24 14:13:26 +0200

    adapt job script

--- job.sh
+++ job.sh
@@ -12,7 +12,7 @@ cp -r $HOME/EWoMan "$TMPDIR"
 #mkdir "$TMPDIR"/output_dir
 
 #Execute a Python program located in $HOME, that takes an input file and output directory as arguments.
-python $TMPDIR/EWoMan/evolution.py --server True --config "diversity_07_roundrobin.json" --num_iter 30 --pop_size 100 --enemies 2 --init_pop "init_pop_enemy2" --seed 111
+python $TMPDIR/EWoMan/evolution.py --server True --config "diversity_075_roundrobin.json" --num_iter 30 --pop_size 100 --enemies 2 --init_pop "init_pop_enemy2" --seed 111
 
 #Copy output directory from scratch to home
-cp -r "$TMPDIR"/EWoMan/diversity_07_* $HOME
+cp -r "$TMPDIR"/EWoMan/diversity_075_* $HOME
