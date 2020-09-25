4f60e8a9ccd94e7f5938c7c4dd9149dce606d072
ChristophHoenes <christoph.hoenes@gmail.com>
2020-09-25 13:47:01 +0200

    fix EvoMan framework bug

--- experiment.py
+++ experiment.py
@@ -5,20 +5,22 @@ import datetime
 from statistics import mean
 import pickle
 from deap import creator, base
-import evolution
+from collections import defaultdict
 
 os.chdir('./evoman_framework')
 sys.path.insert(0, 'evoman')
 from environment import Environment
 # change working directory back to root
 os.chdir('../')
+sys.path.insert(0, './evoman_framework')
+from demo_controller import player_controller
 
 
-def get_best_individuals(enemy = 2):
+def get_best_individuals(enemy=2):
     creator.create("FitnessMax", base.Fitness, weights=(1.0,))
     creator.create("Individual", list, fitness=creator.FitnessMax)
 
-    top_individuals = []
+    top_individuals = defaultdict(list)
 
     seeds = [111, 222, 333, 444, 555, 666, 777, 888, 999, 1010]
     methods = ["method_1", "method2", "method3"]
@@ -34,34 +36,54 @@ def get_best_individuals(enemy = 2):
 
             top5_path = 'results/{}/{}_enemy{}_seed_{}/top5_iter_30'.format(method, prefix, enemy, seed)
             top_ind = pickle.load(open(top5_path, "rb"))[0]
-            top_individuals.append(top_ind)
+            top_individuals[method].append(top_ind)
 
     return top_individuals
 
 if __name__ == "__main__":
 
-    experiment_name = 'experiment'
-    if not os.path.exists(experiment_name):
-        os.makedirs(experiment_name)
-
-    en = 2
-    results = {}
+    enemies = [2,6,7]
+    num_neurons = 10
+    results = []
     count = 0
-    top_individuals = get_best_individuals(enemy=en)
-    for individual in top_individuals:
-        ind_results = []
-        for iter in range(5):
-            env = Environment(experiment_name=experiment_name,
-                              enemies=[en])
-            fit, e_e, e_p, t = env.play(pcont=np.asarray(individual))
-            ind_results.append(e_p-e_e)
-        results[individual] = mean(ind_results)
 
-        print("Count "+str(count))
+    for en in enemies:
+        os.chdir('./evoman_framework')
 
-    with open('experiment_results.csv', 'a') as f:
-        write = csv.writer(f)
-        f.write(str(en))
-        for individual in results.keys():
-            f.write(individual)
+        experiment_name = 'experiment'
+        if not os.path.exists(experiment_name):
+            os.makedirs(experiment_name)
+
+        env = Environment(experiment_name=experiment_name,
+                          enemies=[en],
+                          playermode="ai",
+                          player_controller=player_controller(num_neurons),
+                          enemymode="static",
+                          level=2,
+                          speed="fastest",
+                          randomini="no",
+                          contacthurt="player",
+                          sound="off")
+        os.chdir('../')
+
+        top_individuals = get_best_individuals(enemy=en)
+
+        methods = ["method_1", "method2", "method3"]
+        for method in methods:
+            for individual in top_individuals[method]:
+                ind_results = []
+                for iter in range(5):
+                    os.chdir('./evoman_framework')
+                    fit, e_e, e_p, t = env.play(pcont=np.asarray(individual))
+                    os.chdir('../')
+                    ind_results.append(e_p-e_e)
+                results.append(mean(ind_results))
+
+                print("Count "+str(count))
+
+            with open('experiment_results.csv', 'a') as f:
+                write = csv.writer(f)
+                f.write(str(en))
+                for individual in results:
+                    f.write(str(individual))
 
